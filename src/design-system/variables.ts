71647e762fd17938a91d81f1da383b6490b365d4
André Bezerra <andre.bezerra@lambda3.com.br>
2020-11-12 20:33:57 -0300

    mend

---
+++ src/design-system/variables.ts
@@ -0,0 +1,18 @@
+// https://chir.ag/projects/name-that-color
+const colors = {
+  gorse: "#fff159",
+  mineShaft: "#333333",
+  dustyGray: "#999999",
+  gallery: "#EEEEEE",
+  black: "#000000",
+  white: "#ffffff",
+  transparent: "transparent",
+};
+
+const variables = {
+  colors: {
+    ...colors,
+  },
+};
+
+export default variables;
